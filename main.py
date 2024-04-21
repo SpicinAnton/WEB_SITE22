c2f60ae90a3cf8799f8d108db40dce0566b55bc1
fujiN10 <151755066+fujiN10@users.noreply.github.com>
2024-04-21 18:49:28 +0300

    3

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, redirect
 from flask_login import LoginManager
 from data import db_session
-from forms.user import RegisterForm
+from forms.user import RegisterForm, LoginForm
 from data.users import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
 
 def main():
     db_session.global_init("db/web.db")
@@ -47,7 +48,7 @@ def reqister():
         user.set_password(form.password.data)
         db_sess.add(user)
         db_sess.commit()
-        return redirect('/')
+        return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
 
 
@@ -55,6 +56,22 @@ def reqister():
 def load_user(user_id):
     db_sess = db_session.create_session()
     return db_sess.query(User).get(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            return redirect('/logined')
+    return render_template('login.html', form=form)
+
+
+@app.route('/logined')
+def logined():
+    return render_template('logined.html')
 
 
 if __name__ == '__main__':
