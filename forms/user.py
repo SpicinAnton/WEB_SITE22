f7ed65bcea6f6e33ff6e656bc5da8f0bf057359a
SpicinAnton <151755215+SpicinAnton@users.noreply.github.com>
2024-04-22 00:09:08 +0400

    Update user.py

--- forms/user.py
+++ forms/user.py
@@ -8,4 +8,19 @@ class RegisterForm(FlaskForm):
     password = PasswordField('Пароль', validators=[DataRequired()])
     password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
     name = StringField('Имя пользователя', validators=[DataRequired()])
+    submit = SubmitField('Зарегистрироваться')
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
     submit = SubmitField('Войти')
+
+
+class TovarForm(FlaskForm):
+    name = StringField('Название товара', validators=[DataRequired()])
+    name1 = StringField('Описание товара', validators=[DataRequired()])
+    name2 = StringField('Размеры товара', validators=[DataRequired()])
+    name3 = StringField('Материалы товара', validators=[DataRequired()])
+    prise = StringField('Материалы товара', validators=[DataRequired()])
+    submit = SubmitField('Выставить на продажу')
