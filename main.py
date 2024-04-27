afd342298a9bbb3bd10f1b10fcda5890d4fe6000
fujiN10 <151755066+fujiN10@users.noreply.github.com>
2024-04-27 14:02:10 +0300

    11

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, redirect, request
-from flask_login import LoginManager
+from flask import Flask, render_template, redirect, request, abort
+from flask_login import LoginManager, current_user, login_required, login_user
 from data import db_session
-from forms.user import RegisterForm, LoginForm, TovarForm
+from forms.user import RegisterForm, LoginForm, TovarForm, TovarEditForm
 from data.users import User
-from data. tovars import Tovar, Korzina
+from data.tovars import Tovar
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -66,11 +66,13 @@ def login():
         db_sess = db_session.create_session()
         user = db_sess.query(User).filter(User.email == form.email.data).first()
         if user and user.check_password(form.password.data):
+            login_user(user)
             return redirect('/logined')
     return render_template('login.html', form=form)
 
 
 @app.route('/logined')
+@login_required
 def logined():
     db_sess = db_session.create_session()
     tovars = db_sess.query(Tovar)
@@ -78,6 +80,7 @@ def logined():
 
 
 @app.route('/add', methods=['GET', 'POST'])
+@login_required
 def add():
     form = TovarForm()
     if form.validate_on_submit():
@@ -85,7 +88,8 @@ def add():
         tovar = Tovar(
             name=form.name.data,
             description=form.description.data,
-            price=form.price.data
+            price=form.price.data,
+            user_id=current_user.id
         )
         db_sess.add(tovar)
         db_sess.commit()
@@ -93,25 +97,62 @@ def add():
     return render_template('add.html', form=form)
 
 
-@app.route('/korzina', methods=['GET', 'POST'])
-def korzina():
-    form = TovarForm()
+@app.route('/poisk')
+def poisk():
+    db_sess = db_session.create_session()
+    tovars = db_sess.query(Tovar)
+    text = 'телефон'
+    return render_template('poisk.html', tovars=tovars, text=text)
+
+
+@app.route('/my_tovars')
+@login_required
+def my_tovars():
+    db_sess = db_session.create_session()
+    tovars = db_sess.query(Tovar).filter(Tovar.user_id == current_user.id)
+
+    return render_template('my_tovars.html', tovars=tovars)
+
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit_news(id):
+    form = TovarEditForm()
+    db_sess = db_session.create_session()
+    tovars = db_sess.query(Tovar).filter(Tovar.id == id).first()
+    if request.method == "GET":
+        if tovars:
+            form.name.data = tovars.name
+            form.description.data = tovars.description
+            form.price.data = tovars.price
+        else:
+            abort(404)
     if form.validate_on_submit():
         db_sess = db_session.create_session()
-        tovar = Tovar(
-            name=form.name.data,
-            description=form.description.data,
-            price=form.price.data
-        )
-        db_sess.add(tovar)
+        tovar = db_sess.query(Tovar).filter(Tovar.id == id).first()
+        if tovar:
+            tovar.name = form.name.data
+            tovar.description = form.description.data
+            tovar.price = form.price.data
+            db_sess.commit()
+            return redirect('/my_tovars')
+        else:
+            abort(404)
+
+    return render_template('edit.html', form=form, tovars=tovars)
+
+
+@app.route('/delete/<int:id>', methods=['GET', 'POST'])
+@login_required
+def delete(id):
+    db_sess = db_session.create_session()
+    tovar = db_sess.query(Tovar).filter(Tovar.id == id).first()
+    if tovar:
+        db_sess.delete(tovar)
         db_sess.commit()
-        return redirect('/logined')
-    return render_template('korzina.html')
-
-
-@app.route('/poisk', methods=['GET', 'POST'])
-def poisk():
-    return render_template('poisk.html')
+    else:
+        abort(404)
+    return redirect('/my_tovars')
 
 
 if __name__ == '__main__':
